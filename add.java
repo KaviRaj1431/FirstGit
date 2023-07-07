ad5382f772802dd8e0ef0fc4f328b8aaf7af0ee3
KaviRaj1431 <kratoskavi55741@gmail.com>
2023-07-07 11:32:46 +0530

    First Commit

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class add {
+    public void addition()
+    {
+        int a=10;
+        int b=30;
+       int c=a+b;
+       System.out.println(c);
+    }
+public static void main(String[] args)
+{
+add o=new add();
+o.addition();
+
+}
+}
